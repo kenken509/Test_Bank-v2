6170c05b037780a55ad6ae7ce37c93ab5fd15c2c
kenken509 <ariesllesis050382@gmail.com>
2024-06-08 16:33:57 +0800

    rand

--- resources/js/Pages/Dashboard/TestGenerator/TestGenearator.vue
+++ resources/js/Pages/Dashboard/TestGenerator/TestGenearator.vue
@@ -1,16 +1,16 @@
 <template>
     <DashboardLayout>
-        <Dialog v-model:visible="customModalOpen" modal  :style="{ width: '50rem' }">
+        <Dialog v-model:visible="customModalOpen" modal  :style="{ width: '60rem' }">
             <ModalHeader title="Test Gen">
               <!-- random option check: {{ department[0].subject_codes[0].questions[0].options[0] }} -->
                <!-- prelim: {{ isPrelim }} || midter: {{ isMidterm }} || prefinal: {{ isPrefinal }} || final: {{ isFinal }} -->
                <!--prelim: {{ prelimItems }} || midterm: {{ midTermItems }} || prefinal: {{ preFinalItems }} || final: {{ finalItems }}-->
-              prelim count: {{prelimQuestionCount}} || total items: {{ totalItems }}
-                <form>   
+                question per term: {{ questionPerTerm }}
+                <form @submit.prevent="handlePreviewButtonClicked">   
                     <div class="w-full">
                         <div class="flex w-full pr-4 gap-2 flex-col  md:items-center md:flex-row py-2 ">
                             <label for="department" class="text-black font-semibold w-full max-w-[150px]">Department: </label>
-                            <select id="department" v-model="selectedDepartment" class="rounded-md w-full" >
+                            <select id="department" v-model="selectedDepartment" class="rounded-md w-full"  >
                                 <option value="" hidden disabled>Select department </option>
                                 <option v-for="dep in departmentOptions" :key="dep.id" :value="dep">
                                     {{ dep.name }} 
@@ -41,83 +41,117 @@
                     <div class="w-full m-auto border px-4 bg-blue-100 rounded-md shadow-inner ">
                         <div class="w-full grid grid-cols-5 gap-2 my-2">
                             <div class="col-span-1 flex items-center gap-2 ">
-                                <input v-model="isPrelim" id="prelim_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" />
+                                <input v-model="isPrelim" id="prelim_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" :disabled="!prelimItems.length"/>
                                 <label for="prelim_chkbx" class="hover:cursor-pointer text-black">Prelim</label>
                             </div>
                             <div class="col-span-1 flex items-center ">
                                 <label class="text-black">Print : </label>
                             </div>
                             <div class="col-span-3 flex items-center justify-evenly ">
-                                <input v-model="prelimQuestionCount" class="max-w-[100px] rounded-md mr-2" it="testNum" type="number" max="50" min="0"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="prelimItems.length" :placeholder="prelimItems.length" disabled/>
+                                <input v-model="prelimQuestionCount" class="max-w-[100px] rounded-md mr-2 shadow-md" it="testNum" type="number" max="50" min="0" :disabled="!isPrelim" :class="{'bg-gray-200':!isPrelim}" :required="isPrelim"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="prelimItems.length" :placeholder="prelimItems.length" disabled/>
                             </div>
                         </div>
 
                         <div class="w-full grid grid-cols-5 gap-2 my-2">
                             <div class="col-span-1 flex items-center gap-2 ">
-                                <input v-model="isMidterm" id="midterm_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" />
+                                <input v-model="isMidterm" id="midterm_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" :disabled="!midTermItems.length"/>
                                 <label for="midterm_chkbx" class="hover:cursor-pointer text-black">Mid-term</label>
                             </div>
                             <div class="col-span-1 flex items-center ">
                                 <label class="text-black">Print : </label>
                             </div>
                             <div class="col-span-3 flex items-center justify-evenly ">
-                                <input v-model="midtermQuestionCount" class="max-w-[100px] rounded-md mr-2" it="testNum" type="number" max="50" min="0"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="midTermItems.length" :placeholder="midTermItems.length" disabled/>
+                                <input v-model="midtermQuestionCount" class="max-w-[100px] rounded-md mr-2 shadow-md" it="testNum" type="number" max="50" min="0" :disabled="!isMidterm" :class="{'bg-gray-200':!isMidterm}" :required="isMidterm"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="midTermItems.length" :placeholder="midTermItems.length" disabled/>
                             </div>
                         </div>
 
                         <div class="w-full grid grid-cols-5 gap-2 my-2">
                             <div class="col-span-1 flex items-center gap-2 ">
-                                <input v-model="isPrefinal" id="prefinal_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" />
+                                <input v-model="isPrefinal" id="prefinal_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" :disabled="!preFinalItems.length" />
                                 <label for="prefinal_chkbx" class="hover:cursor-pointer text-black">Pre-final</label>
                             </div>
                             <div class="col-span-1 flex items-center ">
-                                <label class="text-black">Print : </label>
+                                <label class="text-black">Print : sdf{{ isPrefinal }} </label>
                             </div>
                             <div class="col-span-3 flex items-center justify-evenly ">
-                                <input v-model="prefinalQuestionCount" class="max-w-[100px] rounded-md mr-2" it="testNum" type="number" max="50" min="0"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="preFinalItems.length" :placeholder="preFinalItems.length" disabled/>
+                                <input v-model="prefinalQuestionCount" class="max-w-[100px] rounded-md mr-2 shadow-md" it="testNum" type="number" max="50" min="0" :disabled="!isPrefinal" :class="{'bg-gray-200':!isPrefinal}" :required="isPrefinal"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="preFinalItems.length" :placeholder="preFinalItems.length" disabled/>
                             </div>
                         </div>
 
                         <div class="w-full grid grid-cols-5 gap-2 my-2">
-                            <div class="col-span-1 flex items-center gap-2 ">
-                                <input v-model="isFinal" id="final_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" />
-                                <label for="prefinal_chkbx" class="hover:cursor-pointer text-black">Final</label>
+                            <div class="col-span-1 flex items-center gap-2 " >
+                                <input v-model="isFinal" id="final_chkbx" type="checkbox" class="hover:cursor-pointer rounded-sm" value="prelim" :disabled="!finalItems.length" />
+                                <label for="final_chkbx" class="hover:cursor-pointer text-black">Final</label>
                             </div>
                             <div class="col-span-1 flex items-center ">
                                 <label class="text-black">Print : </label>
                             </div>
                             <div class="col-span-3 flex items-center justify-evenly ">
-                                <input v-model="finalQuestionCount" class="max-w-[100px] rounded-md mr-2" it="testNum" type="number" max="50" min="0"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="finalItems.length" :placeholder="finalItems.length" disabled/>
+                                <input v-model="finalQuestionCount" class="max-w-[100px] rounded-md mr-2 shadow-md" it="testNum" type="number" max="50" min="0" :disabled="!isFinal" :class="{'bg-gray-200':!isFinal}" :required="isFinal"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="finalItems.length" :placeholder="finalItems.length" disabled/>
                             </div>
                         </div>
-                      
-                       
+                        <div class="mt-2 ">
+                            <hr class="border border-2 border-gray-400">
+                        </div>
+                        <div class="w-full grid grid-cols-5 gap-2 my-2">
+                            <div class="col-span-2 flex items-center gap-2  ">
+                                <span  class="hover:cursor-pointer text-black">Total Number of Questions: </span>
+                            </div>
+                            
+                            <div class="col-span-3 flex items-center justify-evenly ">
+                                <input v-model="totalQuestionsCount" class="max-w-[100px] rounded-md mr-2 shadow-md" it="testNum" type="number" max="50" min="0" disabled :class="{'bg-gray-200':!totalQuestionsCount}"/>  of  <input class="max-w-[100px] rounded-md ml-2  shadow-md" id="maxTestNum" type="number" :max="finalItems.length" :placeholder="totalItems" disabled/>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mt-2">
-                        <hr>
+                    <div class="mt-4">
+                        <hr class="border border-gray-300">
                     </div>
-                    <div class="mt-2">
+                    <div class="mt-4 flex gap-2 w-full flex-col md:flex-row justify-between px-2" >
                         <div class="flex gap-2  flex-col md:flex-row md:items-center">
-                            <label for="schoolyr">School Year : </label>
-                            <select id="schoolyr" class="rounded-md">
+                            <label for="schoolyr" class="font-semibold">School Year : </label>
+                            <select v-model="selectedSchoolYear"  id="schoolyr" class="rounded-md ">
                                 <option value="" hidden disabled>Select school year</option>
                                 <option v-for="(year,index) in schoolYear" :key="index" >
                                     {{ year }}
                                 </option>
                             </select>
                         </div>
+                        <div class="flex gap-2  flex-col md:flex-row md:items-center">
+                            <label for="term" class="font-semibold">Term: </label>
+                            <select v-model="selectedTerm" id="term" class="rounded-md">
+                                <option value="" hidden disabled>Select a term</option>
+                                <option v-for="(term,index) in terms" :key="index" >
+                                    {{ term }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="flex gap-2  flex-col md:flex-row md:items-center">
+                            <label for="sets" class="font-semibold">Set: </label>
+                            <select v-model="selectedSet" id="sets" class="rounded-md">
+                                <option value="" hidden disabled>Select a set</option>
+                                <option v-for="(set,index) in sets" :key="index" >
+                                    {{ set }}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        <hr class="border border-gray-300">
+                    </div>
+                    <div class="flex flex-col  w-full mt-5 ">
+                        <button @click="handleResetButtonClicked" type="button" class="w-full btn-primary py-2 px-4 m-2 border shadow-md "  >Reset</button>
+                        <button  type="submit" class="w-full btn-primary py-2 px-4 m-2 border shadow-md " >Preview</button>
+                        <button @click="handleSave" type="button" class="w-full btn-primary py-2 px-4 m-2 border shadow-md " >Cancel</button>
+                    </div>
+                    <div class="save-data">
+                        <span>
+                            {{ data.department[0].name }} aries
+                        </span>
                     </div>
                 </form>
             </ModalHeader>
            <!-- <span class="text-red-500"> {{ department }}</span> -->
-            <div class="w-full">
-                <button @click="handleSave" type="button" class="btn-primary" >saves</button>
-            </div>
-            <div class="save-data">
-                <span>
-                    {{ data.department[0].name }} aries
-                </span>
-            </div>
+           
         </Dialog>
         
     </DashboardLayout>
@@ -125,7 +159,7 @@
 
 <script setup>
 import DashboardLayout from '../DashboardLayout.vue';
-import CustomModal from '../../Global Component/CustomModal.vue';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 import ModalHeader from '../Components/ModalHeader.vue'
 import {ref,onMounted, computed,watch} from 'vue'
 import { useForm } from '@inertiajs/vue3';
@@ -186,12 +220,59 @@ const departmentOptions = computed(()=>{
 
     return options
 })
-const selectedTerm = ref('');
+
 
 const isPrelim = ref(false);
+const termQuestionSelection = ref([])
+watch(isPrelim,(val)=>{
+    if(val)
+    {
+        termQuestionSelection.value.push('prelim');
+    }
+    if(!val)
+    {
+        prelimQuestionCount.value = ''
+        termQuestionSelection.value =  termQuestionSelection.value.filter((term)=>term !== 'prelim')
+    }
+})
 const isMidterm = ref(false);
+watch(isMidterm,(val)=>{
+    if(val)
+    {
+        termQuestionSelection.value.push('mid-term');
+    }
+    if(!val)
+    {
+        midtermQuestionCount.value = ''
+        termQuestionSelection.value =  termQuestionSelection.value.filter((term)=>term !== 'mid-term')
+    }
+})
 const isPrefinal = ref(false);
+watch(isPrefinal,(val)=>{
+
+    if(val)
+    {
+        termQuestionSelection.value.push('pre-final');
+    }
+
+    if(!val)
+    {
+        prefinalQuestionCount.value = ''
+        termQuestionSelection.value =  termQuestionSelection.value.filter((term)=>term !== 'pre-final')
+    }
+})
 const isFinal = ref(false);
+watch(isFinal,(val)=>{
+    if(val)
+    {
+        termQuestionSelection.value.push('final');
+    }
+    if(!val)
+    {
+        finalQuestionCount.value = ''
+        termQuestionSelection.value =  termQuestionSelection.value.filter((term)=>term !== 'final')
+    }
+})
 
 const prelimItems   = ref([]);
 const midTermItems  = ref([]);
@@ -236,7 +317,11 @@ watch(finalQuestionCount,(count)=>{
         finalQuestionCount.value = ''
     }
  })
-const totalQuestionCount = ref('')
+const totalQuestionsCount = computed(()=>{
+    let count = ''
+    count = prelimQuestionCount.value+midtermQuestionCount.value+prefinalQuestionCount.value+finalQuestionCount.value
+    return count
+})
 
 watch(selectedSubjectCode,(code)=>{
     
@@ -261,12 +346,18 @@ watch(selectedSubjectCode,(code)=>{
 //watchers
 watch(selectedDepartment,(val)=>{
    
-   
-    selectedSubjectCode.value = '';
-   
+    selectedSubjectCode.value   = '';
+    isPrelim.value              = false
+    isMidterm.value             = false
+    isPrefinal.value            = false
+    isFinal.value               = false
+    selectedSchoolYear.value    = ''
+    selectedTerm.value          = ''
+    selectedSet.value           = ''
    
 })
 
+const selectedSchoolYear = ref('')
 const schoolYear = ref([
     '2024-2025',
     '2025-2026',
@@ -281,7 +372,105 @@ const schoolYear = ref([
     '2034-2035',
 ])
 
+const selectedTerm = ref('')
+const terms = ref([
+    'prelim',
+    'mid-term',
+    'pre-final',
+    'final',
+])
+
+const selectedSet = ref('')
+const sets = ref([
+    'A',
+    'B',
+    'C',
+    'A B',
+    'A C',
+    'A B C',
+])
+
+// buttons logic
+const handleResetButtonClicked = ()=>{
+    selectedDepartment.value=''
+}
+
+const handlePreviewButtonClicked = ()=>{
+    if(!selectedDepartment.value)
+    {
+        errorMessage('Please select a department.')
+        return
+    }
+
+    if(!selectedSubjectCode.value)
+    {
+        errorMessage('Please select a subject code.')
+        return;
+    }
+
+    if(!termQuestionSelection.value.length)
+    {
+        errorMessage('Please select at least 1 term for question selection.')
+        return
+    }
+
+    if(!selectedSchoolYear.value)
+    {
+        errorMessage('Please select a school year.')
+        return
+    }
+
+    if(!selectedTerm.value)
+    {
+        errorMessage("'Please select the exam's term.")
+        return
+    }
+
+    if(!selectedSet.value)
+    {
+        errorMessage("'Please select exam's set.")
+        return
+    }
+
+    
+    console.log('gpt generate an array accoridingly')
+
+    seperateQuestionPerTerm(selectedSubjectCode.value);
+}
+
+
+//sweet alerts
+function errorMessage(message) {
+        customModalOpen.value = false
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message + '!',
+            allowOutsideClick:false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                customModalOpen.value = true
+            }
+        })
+    }
+
+
+// question generation logic
+const questionPerTerm = ref({
+    prelim:[],
+    midTerm:[],
+    prefinal:[],
+    final:[],
+})
+function seperateQuestionPerTerm(code){
+    questionPerTerm.value.prelim = code.questions.filter((question)=> question.term === 'prelim')
+    questionPerTerm.value.midTerm = code.questions.filter((question)=> question.term === 'mid-term')
+    questionPerTerm.value.prefinal = code.questions.filter((question)=> question.term === 'pre-final')
+    questionPerTerm.value.final = code.questions.filter((question)=> question.term === 'final')
+}
 </script>
+
+
 
 <style scoped>
         /* Hide the spinners for WebKit-based browsers (Chrome, Safari) */
