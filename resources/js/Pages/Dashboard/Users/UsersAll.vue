022df08fc33ad8cc6979c719fa35033f8863658d
kenken509 <ariesllesis050382@gmail.com>
2024-06-15 11:55:46 +0800

    search

--- resources/js/Pages/Dashboard/Users/UsersAll.vue
+++ resources/js/Pages/Dashboard/Users/UsersAll.vue
@@ -42,7 +42,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="user in data.users" :key="user.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <tr v-for="user in filteredData" :key="user.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ user.email }}
                             </th>
@@ -155,13 +155,29 @@ import { computed, ref, watch } from 'vue'
 
 
 
-const searchField = ref('')
+
 const data = defineProps({
     users:Array,
     departments:Array,
 })
 
+const searchField = ref('')
 
+const filteredData = computed(()=>{// andito ako 1
+    let searchTerm = searchField.value.toLowerCase().trim()
+    // name email role  if has division division.name if has department department.name 
+    if(!searchTerm)
+    {
+        return data.users
+    }
+    
+    return data.users.filter((user)=>{
+       return user.name.toLowerCase().includes(searchTerm) || user.email.toLowerCase().includes(searchTerm) || user.role.toLowerCase().includes(searchTerm) 
+       || (user.department ? user.department.name.toLowerCase().includes(searchTerm):null) || (user.division ? user.division.name.toLowerCase().includes(searchTerm):null)
+    })
+            
+
+})
 //$table->enum('role', ['admin','co-admin','department head','faculty'])->nullable();
 
 
